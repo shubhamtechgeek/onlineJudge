5f89962ecaca4ef022a87ef7ad3272849ec00fe5
Shubham Gaikwad <shubhamtechgeek@gmail.com>
2023-10-16 21:40:33 +0530

    JWT AUTH -> POST User and User Model

---
+++ backend/models/User.js
@@ -0,0 +1,25 @@
+const mongoose = require("mongoose");
+
+const userSchema = new mongoose.Schema({
+  firstName: {
+    type: String,
+    default: null
+  },
+  lastName: {
+    type: String,
+    default: null
+  },
+  userName: {
+    type: String,
+    unique: true
+  },
+  email: {
+    type: String,
+    unique: true
+  },
+  password: {
+    type: String
+  },
+});
+
+module.exports = mongoose.model("user", userSchema);
