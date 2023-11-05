f5ac228b087a3561f636e1a9575ad7059293b824
Shubham Gaikwad <shubhamtechgeek@gmail.com>
2023-11-05 11:41:28 +0530

    Routes

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -1,20 +1,23 @@
-import logo from './logo.svg';
-import './App.css';
-import Navbar from './Component/Navigation/Navbar';
-import ProblemList from './Component/Problems/ProblemList';
+import "./App.css";
+import { BrowserRouter as Router, Route, Routes } from "react-router-dom";
+import Signup from "./Component/Auth/Signup";
+import HomePage from "./Pages/HomePage";
+import Login from "./Component/Auth/Login";
 
 function App() {
   return (
-    <div style={{
-      backgroundColor: 'black'}}>
-        <div>
-        <Navbar/>
-        </div>
-        <div>
-        <ProblemList/>
-        </div>
-      
-      
+    <div
+      style={{
+        backgroundColor: "black",
+      }}
+    >
+      <Router>
+        <Routes>
+          <Route path="/" element={<HomePage />} />
+          <Route path="/login" element={<Login />} />
+          <Route path="/signup" element={<Signup />} />
+        </Routes>
+      </Router>
     </div>
   );
 }
