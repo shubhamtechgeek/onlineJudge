4dbb025eba6457306094b207ba8e3274f8d3bbf4
Shubham Gaikwad <shubhamtechgeek@gmail.com>
2023-11-20 21:23:46 +0530

    Logo

---
+++ frontend/src/Component/AlgoJudge.js
@@ -0,0 +1,26 @@
+import React from 'react'
+import {Link} from 'react-router-dom'
+
+
+const AlgoJudge = () => {
+    return (
+        <div>
+            <header>
+      <div className="mx-auto max-w-screen-xl  sm:px-6 sm:py-8 lg:px-8">
+        
+          <div className="text-center">
+            <h1 className="text-2xl font-bold text-white sm:text-3xl font-mono">
+              <Link to="/">AlgoJudge</Link>
+            </h1>
+    
+            <p className="mt-1.5 text-sm font-mono text-green-600">
+              This is an online judge which looks good :{")"}
+            </p>
+          </div>
+          </div>
+        
+          </header>
+          </div>)
+}
+
+export default AlgoJudge;
